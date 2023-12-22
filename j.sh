5ac64b4959737258848b4f93bfa765fbea5fc92f
Josphat Mutai <josphatkmutai@gmail.com>
2023-12-22 03:43:04 +0300

    Create j.sh

---
+++ j.sh
@@ -0,0 +1,1 @@
+tailscale up  --advertise-routes=192.168.90.0/24 --login-server https://controlplane.tailscale.com  --authkey tskey-auth-kNrYyv3CNTRL-JL8RsCVgtrEWvdkniFdyrEWgcJVGH4sN --accept-routes --force-reauth
