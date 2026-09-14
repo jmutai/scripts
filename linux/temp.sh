dc8d781b0997fe79c49bd6b1749ae6c7aac1f12e
Josphat Mutai <josphatkmutai@gmail.com>
2026-09-14 20:09:57 +0300

    Comment out tailscale command and add installation script

--- linux/temp.sh
+++ linux/temp.sh
@@ -1,1 +1,3 @@
-tailscale up --auth-key=<key>--ssh
+#tailscale up --auth-key=<key>--ssh
+curl -fsSL https://tailscale.com/install.sh | sh && \
+sudo tailscale up --ssh --auth-key=tskey-auth-kskhHynha421CNTRL-uau4pqfn8nXZN3fLcuq3nXoMNrQcxzS91
