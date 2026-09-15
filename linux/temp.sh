06b53ee265ea63bf9799b2a748ef863bf3e0a6be
Josphat Mutai <josphatkmutai@gmail.com>
2026-09-15 10:57:03 +0300

    Change Tailscale auth key to placeholder
    Updated the Tailscale authentication key placeholder in temp.sh.

--- linux/temp.sh
+++ linux/temp.sh
@@ -1,3 +1,2 @@
-#tailscale up --auth-key=<key>--ssh
 curl -fsSL https://tailscale.com/install.sh | sh && \
-sudo tailscale up --ssh --auth-key=tskey-auth-kskhHynha421CNTRL-uau4pqfn8nXZN3fLcuq3nXoMNrQcxzS91
+sudo tailscale up --ssh --auth-key=<key>
